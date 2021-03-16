e30342df5ad47f4a17e03b09fb5a63909c855f27
melodieyeye <yeyemelodie@outlook.fr>
2021-03-16 09:57:42 +0100

    ajout loader

--- loader.js
+++ loader.js
@@ -1,5 +1,19 @@
-$(window). load(function() {
-    $("#loader").fadeOut(3000, function() {
-        $(".chargement").fadeIn(1000);
-    });
+window.addEventListener("load", function(event) {
+    let loader = document.getElementById("loader");
+    fadeOutIn(loader, 3000);
 });
+
+function fadeOutIn(elem, speed) {
+    if (!elem.style.opacity) {
+        elem.style.opacity = 1;
+    }
+
+    let outInterval = setInterval(function() {
+        elem.style.opacity -= 0.02;
+        if (elem.style.opacity <= 0) {
+            clearInterval(outInterval);
+            let content = document.getElementById("content");
+            content.removeAttribute('class');
+        }
+    }, speed/50 );
+}
